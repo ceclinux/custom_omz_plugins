1eae6c45e5866811fd3305331b510d93f329e01b
ceclinux <src655@gmail.com>
2018-08-02 16:17:59 +0800

    add fetch_text

--- upload_file/upload_file.plugin.zsh
+++ upload_file/upload_file.plugin.zsh
@@ -1,7 +1,26 @@
+UPLOAD_IMAGE_HOST="https://img.vim-cn.com/"
+UPLOAD_TEXT_HOST="https://cfp.vim-cn.com/"
+
 function_name upload_image() {
-  curl -F "name=@$1" https://img.vim-cn.com/
+  curl -F "name=@$1" $UPLOAD_IMAGE_HOST
 }
 
 function_name upload_text() {
-    $@ | curl -F 'vimcn=<-' https://cfp.vim-cn.com/
+  $@ | curl -F 'vimcn=<-' $UPLOAD_TEXT_HOST
+}
+
+function_name fetch_image() {
+  if [ -z "$2" ]; then
+    wget "$UPLOAD_IMAGE_HOST""$1"
+  else
+    wget "$UPLOAD_IMAGE_HOST""$1" -O "$2"
+  fi
+}
+
+function_name fetch_text() {
+  if [ -z "$2" ]; then
+    wget "$UPLOAD_TEXT_HOST""$1"
+  else
+    wget "$UPLOAD_TEXT_HOST""$1" -O "$2"
+  fi
 }
