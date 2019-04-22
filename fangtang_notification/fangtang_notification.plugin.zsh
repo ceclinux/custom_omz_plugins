82afe68dae739afcd17e968e8629a99cd3ac8ece
ruochen.shen <ruochen.shen@thoughtworks.com>
2019-04-22 17:46:23 +0800

    update fangtang notifications: allow indicatiing if success or not

--- fangtang_notification/fangtang_notification.plugin.zsh
+++ fangtang_notification/fangtang_notification.plugin.zsh
@@ -3,9 +3,15 @@
 function_name fangtang() {
   PREV_TIME=$(date +%s)
   $@
+  if [ $? -eq 0 ]
+  then
+    STATUS_MESSAGE="it worked"
+  else
+    STATUS_MESSAGE="it failed"
+  fi
   CURR_TIME=$(date +%s)
   TIME_DIFF=$(( $CURR_TIME - $PREV_TIME ))
-  DESP="desp=""$@"
+  DESP="desp=""**[$STATUS_MESSAGE]** ""$@"
   TEXT="text=一共跑了""$TIME_DIFF""秒"
   curl  -G -s /dev/null  "https://sc.ftqq.com/""$SCKEY"".send" --data-urlencode $DESP --data-urlencode $TEXT
 }
